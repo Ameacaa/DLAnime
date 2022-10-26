c7d8e5ec58c0b441e62a3095a460a7b8d95d4212
Ameacaa <7kevinla@gmail.com>
2022-10-26 21:44:59 +0100

    - Major change for opptimizatio and extract more possible links - still very rarely usable and download not set

--- main.py
+++ main.py
@@ -1,28 +1,84 @@
-import re
-import requests
-from bs4 import BeautifulSoup
+from email.policy import default
 from colorama import Fore, init
+from bs4 import BeautifulSoup
+import requests
+import sys
+import re
 import os
 
-DOWNLOAD_PATH = str(os.path.join(os.path.join(os.environ['USERPROFILE']), 'Videos') + '\\Animes\\')
 
-class PageVideo:
-    def __init__(self, anime_name, episode_number, page_link):
-        self.anime_name     = anime_name
-        self.episode_number = episode_number
-        self.page_link      = page_link
-
-    def __str__(self) -> str:
-        return f'Anime: {self.anime_name} | Number: {self.episode_number} | PageLink: {self.page_link}'
+class Page:
+    def __init__(self, name : str, episode : str, pagelink : str):
+        self.path     :str = str(os.path.join(os.path.join(os.environ['USERPROFILE']), f'Videos\\Animes\\{name}\\'))
+        self.name     :str = name
+        self.episode  :str = episode
+        self.pagelink :str = pagelink
+    def info(self):
+        print(Fore.BLUE + f"{self.name} (Ep:{self.episode:>4}) - {self.pagelink}")
 
 class Video:
-    def __init__(self, path, name, link):
-        self.path = DOWNLOAD_PATH + path + '\\'
-        self.name = name
-        self.link = link
+    def __init__(self, links, page : Page):
+        self.page  :Page = page
+        self.links :list = links
+    def info(self):
+        self.page.info()
+        [print(Fore.CYAN + f'\t- {link}') for link in self.links]
 
-    def __str__(self) -> str:
-        return f'Name: {self.name} | Path: {self.path} | Link: {self.link}'
+
+def GetList(obj):
+    if type(obj) is not list:
+        return [obj]
+    else:
+        return obj
+
+def GetSoup(url):
+    html = requests.get(url).text
+    return BeautifulSoup(html, 'lxml')
+
+def GetTitle(string : str):
+    for r in ['Assistir', 'Legendado', '-', 'Animes', 'Online']:
+        string = string.replace(r, '')    
+    return string.strip()
+
+def GetEpisode(string : str):
+    string = string[string.find('Episódio') + len('Episódio'):].split()[0]
+    return string.strip()
+
+
+def GetPages(urls):
+    urls = GetList(urls)
+    pages = []
+    
+    # Step 1 - Get URLS
+    for url in urls:
+        soup = GetSoup(url)
+        # Get URLS
+        nextPage = soup.find_all('a', 'next')
+        if ( len(nextPage) > 0 ):
+            urls.append(nextPage[0].get('href'))
+        # Get Pages
+        for page in soup.find_all('a', 'col'):
+            name = GetTitle(soup.title.text)
+            ep = GetEpisode(page.get('title'))
+            link = page.get('href')
+            pages.append(Page(name=name, episode=ep, pagelink=link))
+    return pages
+
+def GetVideo(pages):
+    pages = GetList(pages)
+    videos = []
+    for page in pages:
+        html = requests.get(page.pagelink).text
+        urls = []
+        for string in html.splitlines():
+            if string.find('<video') != -1 or string.find('<source') != -1 and string.find('src=') != -1:
+                try:
+                    urls.append(re.search("(?P<url>https?://[^\s]+)", string).group("url"))
+                except:
+                    print(Fore.RED + 'Error in insering video object')
+        videos.append(Video(GetList(urls), page))
+    return videos
+
 
 def DownloadFile(name, path, url):
     name = name + ".mp4"
@@ -39,91 +95,38 @@ def DownloadFile(name, path, url):
 
 
 
-def GetList(obj):
-    if type(obj) is not list:
-        return [obj]
-    else:
-        return obj
+def GetUrl():
+    arg = sys.argv
+    urls = []
+    
+    if len(arg == 1):
+        print('Put some urls in next')
+        quit()
 
-def GetSoup(url):
-    html = requests.get(url).text
-    return BeautifulSoup(html, 'lxml')
+    return [urls.append(arg[c]) for c in range(1, len(arg))]
 
-def GetTitle(string : str):
-    string = string.removeprefix('Assistir')
-    string = string[0:string.find('-')]
-    string = string[0:string.find('Legendado')]
-    return string
+if __name__ == '__main__':
+    BANNER = """
+    ___          _                          ____                      __                __
+   /   |  ____  (_)___ ___  ___  _____     / __ \____ _      ______  / /___  ____ _____/ /
+  / /| | / __ \/ / __ `__ \/ _ \/ ___/    / / / / __ \ | /| / / __ \/ / __ \/ __ `/ __  / 
+ / ___ |/ / / / / / / / / /  __(__  )    / /_/ / /_/ / |/ |/ / / / / / /_/ / /_/ / /_/ /  
+/_/  |_/_/ /_/_/_/ /_/ /_/\___/____/____/_____/\____/|__/|__/_/ /_/_/\____/\__,_/\__,_/   
+                                  /_____/                                                 
+    """
+    
+    os.system("cls")
+    urls = GetUrl()
+    init(autoreset=True)
+    print(Fore.MAGENTA + BANNER)
+    print(Fore.MAGENTA + 'Can take some minutes depending of internet speed and number of videos of the playlist')
+    print('-'*100)
 
-def GetEpisodeNumber(string : str):
-    string = string[string.find('Episódio') + len('Episódio'):].split()[0]
-    return string
+videos, impStrings, pageLinks = GetVideo(GetPages(urls))
 
-# Step 1 - Get all pages with episode selections
-def GetPagesUrl(url):
-    counter = 0
-    pages_urls = GetList(url)
-    while (True):
-        if (counter >= len(pages_urls)):
-            break
-        soup = GetSoup(pages_urls[counter])
-        nextPage = soup.find_all('a', 'next')        
-        if ( len(nextPage) > 0 ):
-            pages_urls.append(nextPage[0].get('href'))
-        counter += 1
-    return list(dict.fromkeys(pages_urls)) # Return a list without duplicate urls
+[video.info() for video in videos]
 
-# Step 2 - Get all link from episodes page selections (return page video link)
-def GetPageVideosUrl(url):
-    pages_url = GetList(url)
-    videos_url = []
-    for page in pages_url:
-        soup = GetSoup(page)
-        videos = soup.find_all('a', 'col')
-        for video in videos:
-            videos_url.append(PageVideo(GetTitle(soup.title.text), GetEpisodeNumber(video.get('title')), video.get('href')))
-    return videos_url
-
-# Step 3 - Get url of the mp4 video
-def GetVideoUrl(url):
-    pages_video = GetList(url)
-    video_url = []
-    for page in pages_video:
-        html = requests.get(page.page_link).text
-        for string in html.splitlines():
-            if string.find('<video') != -1 or string.find('<source') != -1 and string.find('src=') != -1:
-                try:
-                    url = re.search("(?P<url>https?://[^\s]+)", string).group("url")
-                    video_url.append(Video(page.anime_name, page.episode_number, url))
-                except:
-                    print(Fore.RED + 'Error in insering video object')
-    return video_url
-
-
-init(autoreset=True)
-# urls = ['https://goyabu.com/assistir/spy-x-family-parte-2/',
-#         'https://goyabu.com/assistir/death-note-legendado/',
-#         'https://goyabu.com/assistir/black-clover/']
-
-urls = 'https://goyabu.com/assistir/death-note-legendado/'
-
-# Step 1
-print(Fore.MAGENTA + 'Pages founds:')
-pages_urls = GetPagesUrl(urls)
-for pageurl in pages_urls:
-    print(Fore.MAGENTA + '\t-' + pageurl)
-
-# Step 2
-print(Fore.BLUE + 'Pages Video found:')
-pages_video = GetPageVideosUrl(pages_urls)
-for pagevideo in pages_video:
-    print(Fore.BLUE + '\t-' + str(pagevideo))
-
-print(Fore.CYAN + 'Searching video link to download:')
-videos = GetVideoUrl(pages_video)
-for video in videos:
-    #print(Fore.BLUE + video)
-    DownloadFile(name=video.name, path=video.path , url=video.link)
-
+# TODO DOWNLOAD
+# Download
 # TODO BLOB LINK
 # https://stackoverflow.com/questions/48034696/python-how-to-download-a-blob-url-video
